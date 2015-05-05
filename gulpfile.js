d698410b09700697cbb70cd6f32b69764fe78e9f
Marshal <marshal.wu@qq.com>
2015-05-05 23:39:31 +0800

    增加gulp的支持

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function () {
+	var b = browserify({
+	    entries: './script/app.js',
+	    debug: true
+	});
+
+	return b.bundle()
+    .pipe(source('./bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./script'));
+});
+
